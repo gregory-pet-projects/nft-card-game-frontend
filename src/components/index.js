f757be2c9f964cd08fefb20e5aab26b74e7b01c4
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-04 20:07:15 +0800

    created Alert component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import PageHOC from "./PageHOC";
 import CustomInput from "./CustomInput";
 import CustomButton from "./CustomButton";
+import Alert from "./Alert";
 
 
-export { PageHOC, CustomInput, CustomButton };
+export { PageHOC, CustomInput, CustomButton, Alert };
