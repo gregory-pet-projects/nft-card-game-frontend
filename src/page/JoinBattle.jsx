d34db19eb8c7772eb9d0ecf42a2b98c90cd369a0
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-04 20:55:35 +0800

    added HOC component to JoinBattle page

--- src/page/JoinBattle.jsx
+++ src/page/JoinBattle.jsx
@@ -1,5 +1,20 @@
+import { PageHOC } from "../components";
+import { useGlobalContext } from "../context";
+import styles from "../styles";
+
+const TITLE = (
+  <>
+    Join <br />
+    Battle
+  </>
+);
+
+const DESCRIPTION = "Join already existing battles";
+
 const JoinBattle = () => {
+  const {} = useGlobalContext();
+
   return <div>JoinBattle</div>;
 };
 
-export default JoinBattle;
+export default PageHOC(JoinBattle, TITLE, DESCRIPTION);
