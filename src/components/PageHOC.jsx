3a478632970a7364241bf7aa86e0f17ca6ff5a00
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-04 14:16:24 +0800

    created and connected HOC component as a wrapper

---
+++ src/components/PageHOC.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { logo, heroImg } from "../assets";
+import styles from "../styles";
+
+const PageHOC = (Component, title, description) => () => {
+  const navigate = useNavigate();
+  return (
+    <div className={styles.hocContainer}>
+      <div className={styles.hocContentBox}>
+        <img
+          src={logo}
+          alt="logo"
+          className={styles.hocLogo}
+          onClick={() => navigate("/")}
+        />
+        <div className={styles.hocBodyWrapper}>
+          <Component />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PageHOC;
