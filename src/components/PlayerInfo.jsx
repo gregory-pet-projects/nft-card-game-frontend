5c5380aeffe8e16bdfc3e21e714a21eb98baa698
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-07 22:39:58 +0800

    created player info

--- src/components/PlayerInfo.jsx
+++ src/components/PlayerInfo.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import styles from "../styles";
+import ReactTooltip from "react-tooltip";
+import { shortPreviewWalletAddress } from "../utils/service";
 
-const PlayerInfo = () => {
-  return <div>PlayerInfo</div>;
+const healtPoints = 25;
+const healthLevel = (points = 0) =>
+  points >= 12 ? "bg-green-500" : points >= 6 ? "bg-orange-500" : "bg-red-500";
+
+const marginIndexing = (index) => (index !== healtPoints - 1 ? "mr-1" : "mr-0");
+
+const PlayerInfo = ({ player, playerIcon, mt }) => {
+  const health = [...Array(player?.health || 0).keys()];
+  const playerNumber = mt ? "1" : "2";
+  console.log({ health });
+  return (
+    <div className={`${styles.flexCenter} ${mt ? "mt-4" : "mb-4"}`}>
+      <img
+        src={playerIcon}
+        alt="player2"
+        data-tip
+        className="w-14 h-14 object-contain rounded-full"
+        data-for={`Player-${playerNumber}`}
+      />
+      <div
+        data-tip={`Health: ${player?.health}`}
+        data-for={`Health-${playerNumber}`}
+        className={styles.playerHealth}
+      >
+        {health.map((item, idx) => (
+          <div
+            key={`player-item-${item}`}
+            className={`${styles.playerHealthBar} ${healthLevel(
+              player?.health
+            )} ${marginIndexing(idx)}`}
+          />
+        ))}
+      </div>
+      <div
+        data-tip="Mana"
+        data-for={`Mana-${playerNumber}`}
+        className={`${styles.flexCenter} ${styles.glassEffect} ${styles.playerMana}`}
+      >
+        {player?.mana || 0}
+      </div>
+      <ReactTooltip
+        id={`Player-${playerNumber}`}
+        effect="solid"
+        backgroundColor="#7f46f0"
+      >
+        <p className={styles.playerInfo}>
+          <span className={styles.playerInfoSpan}>Name: </span>
+          {player?.playerName}
+        </p>
+        <p className={styles.playerInfo}>
+          <span className={styles.playerInfoSpan}>Address: </span>
+          {shortPreviewWalletAddress(player?.playerAddress)}
+        </p>
+      </ReactTooltip>
+
+      <ReactTooltip
+        id={`Health-${playerNumber}`}
+        effect="solid"
+        backgroundColor="#7f46f0"
+      />
+      <ReactTooltip
+        id={`Mana-${playerNumber}`}
+        effect="solid"
+        backgroundColor="#7f46f0"
+      />
+    </div>
+  );
 };
 
 export default PlayerInfo;
