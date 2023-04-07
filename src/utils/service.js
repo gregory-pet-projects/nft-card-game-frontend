18fc099237228671f68e3d9c8820fe5f1aeaf4df
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-07 19:34:15 +0800

    fixed null fall error

--- src/utils/service.js
+++ src/utils/service.js
@@ -1,5 +1,6 @@
 export const shortPreviewWalletAddress = (address) => {
-  const firstPart = address.slice(0, 5);
-  const lastPart = address.slice(address.length - 5, address.length);
+  if (!address) return "";
+  const firstPart = address?.slice(0, 5);
+  const lastPart = address?.slice(address.length - 5, address.length);
   return `${firstPart}...${lastPart}`;
 };
