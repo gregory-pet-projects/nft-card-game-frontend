98d0f66e779d0820011969ef99bfde5300ffcf18
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-07 19:51:20 +0800

    created join battle page

--- src/page/JoinBattle.jsx
+++ src/page/JoinBattle.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { PageHOC } from "../components";
+import { CustomButton, PageHOC } from "../components";
 import { useGlobalContext } from "../context";
 import styles from "../styles";
 
@@ -14,12 +14,50 @@ const DESCRIPTION = "Join already existing battles";
 
 const JoinBattle = () => {
   const navigate = useNavigate();
-  const {} = useGlobalContext();
+  const { contract, gameData, setShowAlert, setBattleName, walletAddres } =
+    useGlobalContext();
   const createBattleHandler = () => navigate("/create-battle");
+  const isAnyPendingBattles = !!gameData?.pendingBattles?.length;
 
+  const filteredBattle = gameData?.pendingBattles?.filter(
+    (battle) => !battle.players.includes(walletAddres)
+  );
+
+  const handleJoinBattle = async (battleName) => {
+    setBattleName(battleName);
+    setShowAlert({
+      status: true,
+      type: "success",
+      message: `Joining ${battleName}`,
+    });
+    try {
+      await contract.joinBattle();
+    } catch (e) {
+      console.error(e);
+    }
+  };
   return (
     <>
       <h2 className={styles.joinHeadText}>Availabel Battles:</h2>
+      <div className={styles.joinContainer}>
+        {isAnyPendingBattles ? (
+          filteredBattle.map((battle, idx) => (
+            <div className={styles.flexBetween} key={idx}>
+              <p className={styles.joinBattleTitle}>
+                {idx + 1}. {battle.name}
+              </p>
+              <CustomButton
+                title="Join"
+                onClick={() => handleJoinBattle(battle.name)}
+              />
+            </div>
+          ))
+        ) : (
+          <p className={styles.joinLoading}>
+            Reload the page to see new battles
+          </p>
+        )}
+      </div>
       <p className={styles.infoText} onClick={createBattleHandler}>
         Or create a new battle
       </p>
